ababa3fa8b7cc6401b46a566eac12027c61b9b63
Viktoriya Gromova <rockingirl@mail.ru>
2026-06-09 17:49:36 +0500

    data updating Tags commit

--- RTK_Query/lesson-1/src/features/playlists/api/playlistsApi.ts
+++ RTK_Query/lesson-1/src/features/playlists/api/playlistsApi.ts
@@ -28,20 +28,25 @@ export const playlistsApi = createApi({
     // `endpoints` - метод, возвращающий объект с эндпоинтами для `API`, описанными
     // с помощью функций, которые будут вызываться при вызове соответствующих методов `API`
     // (например `get`, `post`, `put`, `patch`, `delete`)
+    tagTypes: ['Playlist'],
     endpoints: build => ({
         // Типизация аргументов (<возвращаемый тип, тип query аргументов (`QueryArg`)>)
         // `query` по умолчанию создает запрос `get` и указание метода необязательно
         fetchPlaylists: build.query<PlaylistsResponse, void>({
             query: () => `playlists`,
+            providesTags: ['Playlist'],
         }),
         createPlaylist: build.mutation<{ data: PlaylistData }, BasePlaylistArgs<CreatePlaylistArgs>>({
-            query: body => ({ method: 'post', url: `playlists`, body,})
+            query: body => ({ method: 'post', url: `playlists`, body,}),
+            invalidatesTags: ['Playlist'],
         }),
         deletePlaylist: build.mutation<void, string>({
-            query: playlistId => ({method: 'delete', url: `playlists/${playlistId}`})
+            query: playlistId => ({method: 'delete', url: `playlists/${playlistId}`}),
+            invalidatesTags: ['Playlist'],
         }),
         updatePlaylist: build.mutation<void, { playlistId: string, body: BasePlaylistArgs<UpdatePlaylistArgs> }>({
-            query: ({playlistId, body}) => ({method: 'put', url: `playlists/${playlistId}`, body})
+            query: ({playlistId, body}) => ({method: 'put', url: `playlists/${playlistId}`, body}),
+            invalidatesTags: ['Playlist'],
         })
     }),
 })
